f6fded8c354ed1e32c6c766b44457a68e9865600
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-01-30 12:37:16 -0600

    Initial Commit

---
+++ FordInterviewPrep/FordInterviewPrep.h
@@ -0,0 +1,18 @@
+//
+//  FordInterviewPrep.h
+//  FordInterviewPrep
+//
+//  Created by Kingsley Enoka on 1/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FordInterviewPrep.
+FOUNDATION_EXPORT double FordInterviewPrepVersionNumber;
+
+//! Project version string for FordInterviewPrep.
+FOUNDATION_EXPORT const unsigned char FordInterviewPrepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FordInterviewPrep/PublicHeader.h>
+
+
---
+++ FordInterviewPrepTests/FordInterviewPrepTests.swift
@@ -0,0 +1,33 @@
+//
+//  FordInterviewPrepTests.swift
+//  FordInterviewPrepTests
+//
+//  Created by Kingsley Enoka on 1/30/21.
+//
+
+import XCTest
+@testable import FordInterviewPrep
+
+class FordInterviewPrepTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
