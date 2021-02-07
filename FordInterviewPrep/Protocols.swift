ef0b217091d63e0f1e6254e49723c2a6c657949c
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-02-06 22:12:55 -0600

    first commit

---
+++ FordInterviewPrep/Protocols.swift
@@ -0,0 +1,8 @@
+//
+//  Protocols.swift
+//  FordInterviewPrep
+//
+//  Created by Kingsley Enoka on 2/2/21.
+//
+
+import Foundation
---
+++ FordInterviewPrep/SortedSet.swift
@@ -0,0 +1,8 @@
+//
+//  SortedSet.swift
+//  FordInterviewPrep
+//
+//  Created by Kingsley Enoka on 1/30/21.
+//
+
+import Foundation
